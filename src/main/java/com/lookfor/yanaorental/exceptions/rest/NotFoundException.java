ee363c50c51269c781b3578ef72562b4346f3e89
Valery Akimov <valery.e.akimov@gmail.com>
2021-05-21 16:41:29 +0300

    Upd: exception handlers

--- src/main/java/com/lookfor/yanaorental/exceptions/rest/NotFoundException.java
+++ src/main/java/com/lookfor/yanaorental/exceptions/rest/NotFoundException.java
@@ -1,5 +1,9 @@
 package com.lookfor.yanaorental.exceptions.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
     static final long serialVersionUID = 1L;
 
--- src/main/java/com/lookfor/yanaorental/exceptions/rest/UserIsNotInRentalException.java
+++ src/main/java/com/lookfor/yanaorental/exceptions/rest/UserIsNotInRentalException.java
@@ -1,5 +1,9 @@
 package com.lookfor.yanaorental.exceptions.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class UserIsNotInRentalException extends RuntimeException {
     static final long serialVersionUID = 1L;
 
